5d62bc637558f6313fbfb2a15fe605d77bd6878a
nguyenkduy <nguyenkduywork@gmail.com>
2021-12-23 12:43:37 +0100

    collision handler finished

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,6 +5,20 @@ public class CollisionHandler : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
-        
+        switch (other.gameObject.tag)
+        {
+            case "Fuel":
+                Debug.Log("Fuel picked up");
+                break;
+            case "Finish":
+                Debug.Log("Finished!");
+                break;
+            case "Obstacle":
+                Debug.Log("You touched an obstacle!!");
+                break;
+            default:
+                Debug.Log("This thing you touched isn't dangerous");
+                break;
+        }
     }
 }
