7778b278537b5c560a848b24f01df5e9a3cf2712
nguyenkduy <nguyenkduywork@gmail.com>
2021-12-23 18:50:43 +0100

    Add level 2

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -11,7 +11,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Fuel picked up");
                 break;
             case "Finish":
-                Debug.Log("Finished!");
+                NextLevel();
                 break;
             case "Obstacle":
                 ReloadLevel();
@@ -26,5 +26,16 @@ public class CollisionHandler : MonoBehaviour
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
